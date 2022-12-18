36efc8d8c00c6d9dc8df561f8a87546ccdf154cc
K-Chinmay <chinmay99v@gmail.com>
2022-12-18 12:55:28 +0530

    cypher bot

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,23 @@ function App() {
   useEffect(() => {
     dispatch(fetchAllQuestions());
     dispatch(fetchAllUsers());
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "3ad3cf2c75f1c82442963aee220936a97",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
   }, [dispatch]);
+
   return (
     <div className="App">
       <Router>
