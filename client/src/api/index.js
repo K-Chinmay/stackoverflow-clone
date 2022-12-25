e8eb9c7fa1f68525e48911188f87054be40d3400
K-Chinmay <chinmay99v@gmail.com>
2022-12-25 19:18:13 +0530

    api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API = axios.create({
-  // baseURL: "https://stackoverflow-server-chinmay.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stackoverflow-server-chinmay.onrender.com",
+  // baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
